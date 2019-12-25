37ad86641870a00193545871b9b839c012f32d72
Boiko Sergii <helvisbox@gmail.com>
2019-12-25 23:29:09 +0200

    Added handlebars and layouts;

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+//Регистрация движка в приложении
+app.engine('hbs', hbs.engine)
+//Использование шаблонизатора
+app.set('view engine', 'hbs')
+//Настройка каталога шаблонов
+app.set('views', 'views/')
+
+app.get('/', (request, response) => {
+    response.render('index')
+})
+
+app.get('/about', (request, response) => {
+    response.render('about')
+})
 
 const PORT = process.env.PORT || 3000
 
