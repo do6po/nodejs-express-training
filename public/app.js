01b5fe2002bb4c8b86240d535c2ed25796313eb4
Boiko Sergii <helvisbox@gmail.com>
2019-12-31 20:21:59 +0200

    New functional : - dynamic deletion courses from card;

--- public/app.js
+++ public/app.js
@@ -1,8 +1,13 @@
-document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('ru-RU', {
+
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru-RU', {
         currency: 'rub',
         style: 'currency',
-    }).format(node.textContent)
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = toCurrency(node.textContent)
 })
 
 const $card = document.querySelector('#card')
@@ -17,7 +22,26 @@ if ($card) {
             })
                 .then(res => res.json())
                 .then(card => {
-                    console.log(card)
+                    if (card.courses.length) {
+                        const html = card.courses.map(c => {
+                            return `
+                                <tr>
+                                    <th>${c.title}</th>
+                                    <th>${c.count}</th>
+                                    <th>
+                                        <button class="btn btn-small js-remove" data-id="${c.id}">
+                                            Удалить
+                                        </button>
+                                    </th>
+                                </tr>
+                            `
+                        }).join('')
+
+                        $card.querySelector('tbody').innerHTML = html
+                        $card.querySelector('.price').textContent = toCurrency(card.price)
+                    } else {
+                        $card.innerHTML = '<p> Корзина пуста </p>'
+                    }
                 })
         }
     })
